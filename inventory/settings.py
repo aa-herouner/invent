3b4fc8a461cc41f42d311b8d356012497a0a121a
aa-herouner <ahmadabubakarharuna53@gmail.com>
2025-05-05 16:43:46 +0100

    Changes to navbar and static line

--- inventory/settings.py
+++ inventory/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'inventory/static')] 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 
 AUTH_USER_MODEL = 'users.User'
